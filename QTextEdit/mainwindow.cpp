b57c32081da374366b8994f2e1db9df012b49b8d
z-shaffer <zshaffer@protonmail.com>
2024-02-20 15:24:53 +0100

    Implemented methods for toolbar

--- QTextEdit/mainwindow.cpp
+++ QTextEdit/mainwindow.cpp
@@ -6,9 +6,110 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+// Toolbar Methods //
+
+void MainWindow::on_actionNew_triggered()
+{
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+
+void MainWindow::on_actionOpen_triggered()
+{
+    // Loading text file in
+    QString fileName = QFileDialog::getOpenFileName(this, "Open file");
+    QFile file(fileName);
+    currentFile = fileName;
+    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Can not open file : " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+
+    // Reading text and writing it to the editor
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+
+void MainWindow::on_actionSave_as_triggered()
+{
+    // Render save as window
+    QString fileName = QFileDialog::getSaveFileName(this, "Save as");
+    QFile file(fileName);
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Can not save file : " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+
+    // Output to a new text file
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+
+void MainWindow::on_actionPrint_triggered()
+{
+    // Allow user to select a printer
+    QPrinter printer;
+    printer.setPrinterName("Printer name");
+    QPrintDialog pDialog(&printer, this);
+    if (pDialog.exec() == QDialog::Rejected) {
+        QMessageBox::warning(this, "Warning", "Cannot access printer");
+        return;
+    }
+
+    // Transmit text to printer
+    ui->textEdit->print(&printer);
+}
+
+
+void MainWindow::on_actionExit_triggered()
+{
+    QApplication::quit();
+}
+
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
