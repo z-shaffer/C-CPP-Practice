3420eea00ac01c807562cb861fb75b990549d566
z-shaffer <zshaffer@protonmail.com>
2024-02-27 16:45:40 +0100

    Main window header finished

--- painTQ/mainwindow.h
+++ painTQ/mainwindow.h
@@ -1,7 +1,10 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QList>
 #include <QMainWindow>
+
+class ScribbleArea;
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +22,32 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    void createActions();
+    void createMenus();
+    bool maybeSave();
+    bool saveFile(const QByteArray &fileFormat);
+    ScribbleArea *scribbleArea;
+    QMenu *saveAsMenu();
+    QMenu *fileMenu;
+    QMenu *optionMenu;
+    QMenu *helpMenu;
+    QAction *openAct;
+    QList<QAction *> saveAsActs;
+    QAction *penColorAct;
+    QAction *penWidthAct;
+    QAction *printAct;
+    QAction *clearScreenAct;
+    QAction *aboutAct;
+    QAction *aboutQtAct;
+
+private slots:
+    void open();
+    void save();
+    void penColor();
+    void penWidth();
+    void about();
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
 };
 #endif // MAINWINDOW_H
